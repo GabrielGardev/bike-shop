a56420f9274059740dba5e715430e0ae4d65df6b
GabrielGardev <gabriel.gurdev@gmail.com>
2019-09-30 18:45:27 +0300

    first upload

---
+++ src/main/java/bikeshop/BikeShopApplication.java
@@ -0,0 +1,13 @@
+package bikeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeShopApplication.class, args);
+    }
+
+}
