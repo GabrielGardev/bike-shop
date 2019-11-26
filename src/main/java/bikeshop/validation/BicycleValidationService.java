7aa9c979c6a162b0c8fc8a71bfcbcb448bf8c4c2
GabrielGardev <gabriel.gurdev@gmail.com>
2019-11-26 18:07:27 +0200

    refactor validations

--- src/main/java/bikeshop/validation/BicycleValidationService.java
+++
@@ -1,7 +0,0 @@
-package bikeshop.validation;
-
-import bikeshop.domain.entities.Bicycle;
-
-public interface BicycleValidationService {
-    boolean isValid(Bicycle bicycle);
-}
--- src/main/java/bikeshop/validation/UserValidationService.java
+++
@@ -1,7 +0,0 @@
-package bikeshop.validation;
-
-import bikeshop.domain.entities.User;
-
-public interface UserValidationService {
-    boolean isValid(User user);
-}
