0ad10c2664b8cbcb486b17617f7a39f23f9e2cda
GabrielGardev <gabriel.gurdev@gmail.com>
2020-08-03 15:10:19 +0300

    add security config for swagger

--- src/main/java/bikeshop/config/ApplicationSecurityConfiguration.java
+++ src/main/java/bikeshop/config/ApplicationSecurityConfiguration.java
@@ -3,6 +3,7 @@ package bikeshop.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -36,6 +37,17 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .and()
                     .exceptionHandling()
                     .accessDeniedPage("/");
+    }
+
+    //enable swagger doc access unauthorized
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
     }
 
     private CsrfTokenRepository csrfTokenRepository() {
